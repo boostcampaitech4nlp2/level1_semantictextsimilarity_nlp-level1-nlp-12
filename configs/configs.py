7828ba7e6c02601696b6ead046e8254d4fb1e787
GinToFim <shlf6673@gmail.com>
2022-10-26 10:34:38 +0900

    [UPDATE] model and train

--- configs/configs.py
+++ configs/configs.py
@@ -4,22 +4,22 @@ parser = argparse.ArgumentParser(description="pstage")
 
 ### WandB ###
 parser.add_argument(
-    "--wandb_project", default="boostcamp_practice", type=str, help="wandb 레포지토리 이름"
+    "--wandb_project", default="Text_aug", type=str, help="wandb 레포지토리 이름"
 )
-parser.add_argument("--name", default="kbh", type=str, help="작성자 이름")
+parser.add_argument("--name", default="kjh", type=str, help="작성자 이름")
 parser.add_argument("--info", default="데이터증강", type=str, help="추가한 작업 정보")
 
 ### Model ###
 parser.add_argument(
     "--model_name",
     type=str,
-    default="klue/roberta-small",
-    help="klue/roberta-small | 다른 이름",
+    default="beomi/KcELECTRA-base",
+    help="klue/roberta-small | monologg/koelectra-base-v3-discriminator | beomi/KcELECTRA-base",
 )
 
 ### HyperParmeters ###
-parser.add_argument("--batch_size", default=16, type=int)
-parser.add_argument("--max_epoch", default=3, type=int)
+parser.add_argument("--batch_size", default=32, type=int)
+parser.add_argument("--max_epoch", default=10, type=int)
 parser.add_argument("--shuffle", default=True)
 
 ### optimizer ###
@@ -35,9 +35,10 @@ parser.add_argument(
 )
 
 ### Data Path ###
-parser.add_argument("--train_path", default="./data/raw_data/train.csv")
-parser.add_argument("--dev_path", default="./data/raw_data/dev.csv")
-parser.add_argument("--test_path", default="./data/raw_data/dev.csv")
-parser.add_argument("--predict_path", default="./data/raw_data/test.csv")
+parser.add_argument("--train_path", default="./data/train.csv")
+parser.add_argument("--aug_train_path", default="./data/aug_train.csv")
+parser.add_argument("--dev_path", default="./data/dev.csv")
+parser.add_argument("--test_path", default="./data/dev.csv")
+parser.add_argument("--predict_path", default="./data/test.csv")
 
 config = parser.parse_args()
