78710a062f8ef4047fb5b90198d37f56771acb4e
soypabloo <kate49143@gmail.com>
2022-10-26 06:47:51 +0000

    [fix] adapt default callback -> early_stop_callback

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -6,13 +6,18 @@ class Trainer(pl.Trainer):
     def __init__(
         self, accelerator, devices, max_epochs, log_every_n_steps, wandb_logger
     ):
+        early_stop_callback = EarlyStopping(
+            monitor="val_loss",
+            min_delta=0.00,
+            patience=3,
+            verbose=False,
+            mode="min",
+        )
         super(Trainer, self).__init__(
             accelerator=accelerator,
             devices=devices,
             max_epochs=max_epochs,
             log_every_n_steps=log_every_n_steps,
             logger=wandb_logger,
-        )
-        early_stop_callback = EarlyStopping(
-            monitor="val_loss", min_delta=0.00, patience=3, verbose=False, mode="min"
+            callbacks=early_stop_callback,
         )
